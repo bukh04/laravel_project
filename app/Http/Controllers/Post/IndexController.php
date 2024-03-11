a3952e38e705e9f69b39aedcb9813343e2e54112
Vlad <buhanevych04@ukr.net>
2024-03-11 19:28:03 +0100

    added filtration

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers\Post;
 
+use App\Filters\PostFilter;
 use App\Models\Category;
 use App\Models\Post;
 
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(PostFilter $filter)
     {
-        $posts = Post::paginate(10);
+        $posts = Post::filter($filter)->paginate(10);
         $categories = Category::all();
         return view('post.index', compact('posts','categories'));
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use App\Models\Traits\Filterable;
+use App\Filters\QueryFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -24,6 +25,10 @@ class Post extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class, 'post_tags', 'post_id', 'tag_id');
+    }
+    public function scopeFilter(Builder $builder, QueryFilter $filter)
+    {
+        return $filter->apply($builder);
     }
 
 }
